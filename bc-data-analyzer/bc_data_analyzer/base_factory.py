27b30bab6dbe073b62281d9b27762016e77ceb0c
Daniel Perez <daniel@perez.sh>
2020-05-25 15:57:01 +0200

    Remove unused code

--- bc-data-analyzer/bc_data_analyzer/base_factory.py
+++ bc-data-analyzer/bc_data_analyzer/base_factory.py
@@ -2,14 +2,6 @@ from functools import lru_cache
 from abc import ABCMeta
 
 
-class classproperty(object):
-    def __init__(self, fget):
-        self.fget = fget
-
-    def __get__(self, owner_self, owner_cls):
-        return self.fget(owner_cls)
-
-
 class FactoryMeta(ABCMeta):
     @property
     def _entities(cls):
