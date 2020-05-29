7d8974057e188833983e8f24fa9a7bd8adc3d216
Daniel Perez <daniel@perez.sh>
2020-05-29 09:49:00 +0200

    Support bulk format

--- bc-data-analyzer/bc_data_analyzer/data_reader.py
+++ bc-data-analyzer/bc_data_analyzer/data_reader.py
@@ -5,6 +5,8 @@ import json
 def read_actions_over_time(filename: str):
     with open(filename) as f:
         data = json.load(f)
+    if "Results" in data and "GroupedActionsOverTime" in data["Results"]:
+        data = data["Results"]["GroupedActionsOverTime"]
     actions_over_time = []
     for key, value in data["Actions"].items():
         parsed_time = dt.datetime.fromisoformat(key.rstrip("Z"))
