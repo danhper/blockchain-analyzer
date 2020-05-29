bd1ae4bc6dc6dace93cf9099ec0fe340bf659b6f
Daniel Perez <daniel@perez.sh>
2020-05-29 15:13:01 +0200

    Fix EOS plot

--- bc-data-analyzer/bc_data_analyzer/blockchains/eos.py
+++ bc-data-analyzer/bc_data_analyzer/blockchains/eos.py
@@ -17,10 +17,10 @@ class EOS(Blockchain):
             settings.PACKAGE_NAME, "data/eos-categories.json")
         self.categories = json.loads(categories)
         self.category_indexes = {
-            category: i for i, category in enumerate(self.categories["names"])}
+            category["name"]: i for i, category in enumerate(self.categories["categories"])}
 
     def compute_categories(self, actions_count):
-        category_counts = [0 for _ in self.categories["names"]]
+        category_counts = [0 for _ in self.categories["categories"]]
         for action in actions_count["Actions"]:
             category = self.categories["mapping"].get(action["Name"], "others")
             category_counts[self.category_indexes[category]] += action["Count"]
@@ -29,8 +29,8 @@ class EOS(Blockchain):
     def transform_actions_over_time(self, actions: List[Tuple[dt.datetime, dict]]) \
             -> Tuple[List[dt.datetime], List[str], np.ndarray, List[str]]:
 
-        labels = [a.capitalize() for a in self.categories["names"]]
+        labels = [a["name"].capitalize() for a in self.categories["categories"]]
         dates = [a[0] for a in actions]
         ys = zip(*[self.compute_categories(a) for _, a in actions])
-        colors = plot_utils.make_palette("green", "gray", "teal", "violet", "blue", "brown")
+        colors = plot_utils.make_palette(*[a["color"] for a in self.categories["categories"]])
         return labels, dates, ys, colors
